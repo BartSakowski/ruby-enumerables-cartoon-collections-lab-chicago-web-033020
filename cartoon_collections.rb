c903914705fe0ff90f2816b8f30895066e9b1c45
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-23 23:31:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves_array) # code an argument here
+
+  dwarves_array.each_with_index { |item, index| p "#{index+1}." "#{item}" }
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! { |planeteer| planeteer.capitalize!}
+  planeteer_calls.collect { |planeteer| planeteer + "!" }
+
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_array)
+
+  calls_array.any? { |word| word.length > 3 }
+
+
 end
 
 def find_the_cheese# code an argument here
