604c0af348d3384d8208ad4bf04ed363d294b20a
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-23 23:57:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,9 @@ end
 def long_planeteer_calls(calls_array)
 
   if calls_array.any? { |word| word.length > 4} == true
-    return true
+
   else call_array.all? { |word| word.length >= 4} == false
-    return false
+    
   end
 # else calls_array.all? { |word| word.length > 4 }
 
