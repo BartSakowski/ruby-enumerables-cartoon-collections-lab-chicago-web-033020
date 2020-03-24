c7255b9b78e883f21372d91ce870dc5d23a1d40a
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-24 00:09:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(food_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   if food_array.include?(cheese_types) == true
-    return food_array.detet {|i| i.cheese_types?}
+    return food_array.detet {|food| food.cheese_types?}
   else food_array.include?(cheese_types) == false
     return nil
   end
