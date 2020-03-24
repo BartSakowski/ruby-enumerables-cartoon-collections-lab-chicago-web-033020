c9a605006fa1a0fd20f0971f6083c41893f5c5b3
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-24 00:12:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def roll_call_dwarves(dwarves_array) # code an argument here
 
@@ -22,7 +22,7 @@ def long_planeteer_calls(calls_array)
   end
 
 end
-
+require 'pry'
 
 def find_the_cheese(food_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
